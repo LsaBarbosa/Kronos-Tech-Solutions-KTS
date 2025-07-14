47427cf1606175ef24a9b7b8d1c40fbce926cc22
LsaBarbosa <lsa94.contato@gmail.com>
2025-07-14 17:21:18 -0300

    geração de relatório de horas

--- src/main/java/com/kts/kronos/application/port/in/usecase/TimeRecordUseCase.java
+++ src/main/java/com/kts/kronos/application/port/in/usecase/TimeRecordUseCase.java
@@ -1,8 +1,8 @@
 package com.kts.kronos.application.port.in.usecase;
-
 import com.kts.kronos.adapter.in.web.dto.timerecord.*;
 import com.kts.kronos.domain.model.StatusRecord;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
@@ -13,7 +13,8 @@ public interface TimeRecordUseCase {
     void updateStatus(UpdateTimeRecordStatusRequest request);
     void toggleActivate(ToggleActivate toggleActivate);
     void deleteTimeRecord(DeleteTimeRecordRequest req);
-    SimpleReportResponse reportResumido(SimpleReportRequest req);
+    SimpleReportResponse simpleReport(SimpleReportRequest req);
+    byte[] simpleReportPDF(SimpleReportResponse report) throws IOException;
     TimeRecordResponse updateTimeRecord(UpdateTimeRecordRequest req);
     List<TimeRecordResponse> listReport(UUID employeeId,
                                         String reference,
--- src/main/java/com/kts/kronos/application/port/out/repository/CompanyRepository.java
+++ src/main/java/com/kts/kronos/application/port/out/repository/CompanyRepository.java
@@ -4,11 +4,13 @@ import com.kts.kronos.domain.model.Company;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface CompanyRepository {
     Company save(Company company);
     Optional<Company> findByCnpj(String cnpj);
     List<Company> findAll();
+    Optional<Company> findById(UUID company);
     List<Company> findByActive(boolean active);
     void deleteByCnpj(String cnpj);
 }
