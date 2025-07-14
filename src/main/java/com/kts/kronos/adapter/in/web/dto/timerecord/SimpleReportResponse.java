f23f5378882a4184d124e4ecf2d78db20ba3704e
LsaBarbosa <lsa94.contato@gmail.com>
2025-07-14 17:21:18 -0300

    geração de relatório de horas

--- src/main/java/com/kts/kronos/adapter/in/web/dto/timerecord/SimpleReportResponse.java
+++ src/main/java/com/kts/kronos/adapter/in/web/dto/timerecord/SimpleReportResponse.java
@@ -3,6 +3,8 @@ package com.kts.kronos.adapter.in.web.dto.timerecord;
 import java.util.List;
 
 public record SimpleReportResponse(
+        String enployeeName,
+        String companyName,
         List<SimpleReportDay> days,
         String totalHoursWorked,
         String totalBalance
