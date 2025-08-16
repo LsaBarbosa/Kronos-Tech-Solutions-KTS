940448b4b29cc870a07a30414502bc76c6a96ef7
LsaBarbosa <lsa94.contato@gmail.com>
2025-08-16 02:05:56 -0300

    adicionando preAuthoirize nos endpoints

--- src/main/java/com/kts/kronos/adapter/in/web/http/UserController.java
+++ src/main/java/com/kts/kronos/adapter/in/web/http/UserController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -25,23 +26,27 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('MANAGER')")
     public void registerUser(@Valid @RequestBody CreateUserRequest dto) {
         useCase.createUser(dto);
     }
 
     @GetMapping(USER_BY_USERNAME)
+    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<UserResponse> getUser(@PathVariable String userName) {
         var user = useCase.getUserByUsername(userName);
         return ResponseEntity.ok(UserResponse.fromDomain(user));
     }
 
     @GetMapping(USER_BY_ID)
-    public ResponseEntity<UserResponse> getUserId(@PathVariable UUID userId) {
+    @PreAuthorize("hasAnyRole('MANAGER', 'PARTNER')")
+    public ResponseEntity<UserResponse> getUserId(@RequestParam(required = false) UUID userId) {
         var user = useCase.getUserById(userId);
         return ResponseEntity.ok(UserResponse.fromDomain(user));
     }
 
     @GetMapping(USERS)
+    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<UserListResponse> allUsers(
             @RequestParam(value = "active", required = false) Boolean active
     ) {
@@ -52,23 +57,26 @@ public class UserController {
 
     @PatchMapping(UPDATE_USER)
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('MANAGER')")
     public void updateUser(@PathVariable UUID userId, @Valid @RequestBody UpdateUserRequest dto) {
         useCase.updateUser(userId, dto);
     }
 
     @PatchMapping(TOGGLE_ACTIVATE_USER)
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('MANAGER')")
     public void activateUser(@PathVariable UUID id) {
         useCase.toggleActivate(id);
     }
 
     @DeleteMapping(DELETE_USER)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-
+    @PreAuthorize("hasRole('MANAGER')")
     public void deleteCompany(@PathVariable UUID id) {
         useCase.deleteUser(id);
     }
 
+    @PreAuthorize("hasAnyRole('MANAGER', 'PARTNER')")
     @PutMapping(PASSWORD)
     public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRequest req) {
         useCase.changeOwnPassword(req);
