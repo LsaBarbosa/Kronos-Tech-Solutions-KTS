a9e7589b0563cffc92ee52f5c35de3f7e0709312
LsaBarbosa <lsa94.contato@gmail.com>
2025-09-14 16:46:44 -0300

    msg

--- src/main/java/com/kts/kronos/application/port/out/provider/MessageProvider.java
+++ src/main/java/com/kts/kronos/application/port/out/provider/MessageProvider.java
@@ -1,6 +1,7 @@
 package com.kts.kronos.application.port.out.provider;
 import com.kts.kronos.domain.model.Message;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +11,5 @@ public interface MessageProvider {
     Optional<Message> findById(UUID messageId);
     List<Message> findByCompanyId(UUID companyId);
     void deleteByMessageIdAndEmployeeId(UUID messageId, UUID employeeId);
+    void deleteByCreationDateBefore(LocalDateTime threshold);
 }
