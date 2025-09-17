054f1658595e722ca05ac5ddbe59ea65910b3bb8
LsaBarbosa <lsa94.contato@gmail.com>
2025-09-17 00:36:44 -0300

    RENDER Settings

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# Estágio de build
+FROM gradle:jdk21 AS build
+WORKDIR /app
+COPY . .
+RUN gradle bootJar
+
+# Estágio de execução
+FROM openjdk:21-slim
+WORKDIR /app
+COPY --from=build /app/build/libs/kronos-0.0.1-SNAPSHOT.jar ./app.jar
+EXPOSE 8080
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/kts/kronos/config/SecurityConfig.java
+++ src/main/java/com/kts/kronos/config/SecurityConfig.java
@@ -15,6 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -50,6 +56,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Permite requisições do seu front-end Vercel
+        configuration.setAllowedOrigins(List.of("https://kronos-tech-solution-user-plataform.vercel.app/*", "https://kts-rymjdntshgrveaf-mhnsgbrdefwc-thnsgrbaefc.vercel.app/*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
