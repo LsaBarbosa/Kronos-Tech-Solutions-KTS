0a2bd20a926398a492e8fcea457aace6755bd396
LsaBarbosa <lsa94.contato@gmail.com>
2025-09-30 15:18:39 -0300

    builds

--- src/main/java/com/kts/kronos/adapter/out/persistence/impl/BucketStorageProviderImpl.java
+++ src/main/java/com/kts/kronos/adapter/out/persistence/impl/BucketStorageProviderImpl.java
@@ -1,19 +1,18 @@
 package com.kts.kronos.adapter.out.persistence.impl;
 
-import com.kts.kronos.application.port.out.provider.GcsStorageProvider;
+import com.kts.kronos.application.port.out.provider.BucketStorageProvider;
 import com.google.cloud.storage.*;
-import com.kts.kronos.application.port.out.provider.GcsStorageProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class GcsStorageProviderImpl implements GcsStorageProvider {
+public class BucketStorageProviderImpl implements BucketStorageProvider {
     private final Storage storage;
     private final String bucketName;
 
-    public GcsStorageProviderImpl(@Value("${gcp.storage.bucket-name}") String bucketName) {
+    public BucketStorageProviderImpl( @Value("${aws.s3.bucket-name}")  String bucketName) {
         // Inicializa o cliente Storage, que usa a autenticação padrão do ambiente.
         this.storage = StorageOptions.getDefaultInstance().getService();
         this.bucketName = bucketName;
@@ -21,7 +20,7 @@ public class GcsStorageProviderImpl implements GcsStorageProvider {
 
     @Override
     public String uploadFile(String objectName, byte[] fileData, String contentType) {
-        log.info("Iniciando upload para GCS: bucket={}, object={}", bucketName, objectName);
+        log.info("Iniciando upload para aws: bucket={}, object={}", bucketName, objectName);
         try {
             BlobId blobId = BlobId.of(bucketName, objectName);
             BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
@@ -30,46 +29,46 @@ public class GcsStorageProviderImpl implements GcsStorageProvider {
 
             // Sobe o arquivo
             storage.create(blobInfo, fileData);
-            log.info("Upload para GCS concluído: object={}", objectName);
+            log.info("Upload para aws concluído: object={}", objectName);
             return objectName;
         } catch (StorageException e) {
-            log.error("Erro no upload do arquivo para GCS: {}", e.getMessage(), e);
-            throw new RuntimeException("Falha ao fazer upload para o Google Cloud Storage.", e);
+            log.error("Erro no upload do arquivo para aws: {}", e.getMessage(), e);
+            throw new RuntimeException("Falha ao fazer upload para o Bucket.", e);
         }
     }
 
     @Override
     public byte[] downloadFile(String objectName) {
-        log.info("Iniciando download do GCS: bucket={}, object={}", bucketName, objectName);
+        log.info("Iniciando download do aws: bucket={}, object={}", bucketName, objectName);
         try {
             BlobId blobId = BlobId.of(bucketName, objectName);
             Blob blob = storage.get(blobId);
 
             if (blob == null) {
-                throw new StorageException(404, "Arquivo não encontrado no GCS: " + objectName);
+                throw new StorageException(404, "Arquivo não encontrado no aws: " + objectName);
             }
 
             // Baixa o conteúdo
             byte[] content = blob.getContent();
 
-            log.info("Download do GCS concluído: object={}", objectName);
+            log.info("Download do aws concluído: object={}", objectName);
             return content;
         } catch (StorageException e) {
-            log.error("Erro no download do arquivo do GCS: {}", e.getMessage(), e);
+            log.error("Erro no download do arquivo do aws: {}", e.getMessage(), e);
             throw new RuntimeException("Falha ao fazer download do Google Cloud Storage.", e);
         }
     }
 
     @Override
     public void deleteFile(String objectName) {
-        log.info("Iniciando exclusão do GCS: bucket={}, object={}", bucketName, objectName);
+        log.info("Iniciando exclusão do aws: bucket={}, object={}", bucketName, objectName);
         try {
             BlobId blobId = BlobId.of(bucketName, objectName);
             storage.delete(blobId);
-            log.info("Exclusão do GCS concluída: object={}", objectName);
+            log.info("Exclusão do aws concluída: object={}", objectName);
         } catch (StorageException e) {
-            log.error("Erro na exclusão do arquivo do GCS: {}", e.getMessage(), e);
-            throw new RuntimeException("Falha ao excluir o arquivo do Google Cloud Storage.", e);
+            log.error("Erro na exclusão do arquivo do aws: {}", e.getMessage(), e);
+            throw new RuntimeException("Falha ao excluir o arquivo do BucketS3.", e);
         }
     }
 }
