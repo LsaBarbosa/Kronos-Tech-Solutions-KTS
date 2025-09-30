1ad90f656f3c33a8e81b141e88832f46623bfdec
LsaBarbosa <lsa94.contato@gmail.com>
2025-09-30 17:49:51 -0300

    builds

--- src/main/java/com/kts/kronos/adapter/in/web/dto/message/publisher/TimeRecordChangePublisher.java
+++ src/main/java/com/kts/kronos/adapter/in/web/dto/message/publisher/TimeRecordChangePublisher.java
@@ -1,27 +1,33 @@
 package com.kts.kronos.adapter.in.web.dto.message.publisher;
 
-import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.kts.kronos.adapter.in.messaging.dto.TimeRecordChangeRequestMessage;
+
+
+import io.awspring.cloud.sns.core.SnsTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import static com.kts.kronos.constants.Messages.TIME_RECORD_APPROVAL_TOPIC;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class TimeRecordChangePublisher {
-    private final PubSubTemplate pubSubTemplate;
+    private final SnsTemplate snsTemplate;
+
+    // Injeta o ARN do Tópico da configuração
+    @Value("${time-record.approval-topic-arn}")
+    private String timeRecordApprovalTopicArn;
 
     public void publishApprovalRequest(TimeRecordChangeRequestMessage message) {
         try {
-            // Publica a mensagem. O Spring Cloud GCP a serializa para JSON/Bytes.
-            pubSubTemplate.publish(TIME_RECORD_APPROVAL_TOPIC, message);
-            log.info("Solicitação de alteração de ponto ID {} publicada no tópico {}",
-                    message.timeRecordId(), TIME_RECORD_APPROVAL_TOPIC);
+            // Publica a mensagem no ARN do tópico. O Spring Cloud AWS faz a serialização JSON.
+            snsTemplate.convertAndSend(timeRecordApprovalTopicArn, message);
+
+            log.info("Solicitação de alteração de ponto ID {} publicada no tópico SNS {}",
+                    message.timeRecordId(), timeRecordApprovalTopicArn);
         } catch (Exception e) {
-            log.error("Falha ao publicar a mensagem no Pub/Sub para o registro ID {}: {}",
+            log.error("Falha ao publicar a mensagem no AWS SNS para o registro ID {}: {}",
                     message.timeRecordId(), e.getMessage());
             throw new RuntimeException("Falha na comunicação com o serviço de mensageria.", e);
         }
