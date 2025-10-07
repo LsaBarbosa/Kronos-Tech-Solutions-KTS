3931206f3a36bebc9454f5a924c6d1f9845fde25
LsaBarbosa <lsa94.contato@gmail.com>
2025-10-06 22:29:11 -0300

    ymal frontend variabel fix

--- src/main/java/com/kts/kronos/KronosApplication.java
+++ src/main/java/com/kts/kronos/KronosApplication.java
@@ -2,12 +2,14 @@ package com.kts.kronos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableJpaRepositories("com.kts.kronos.adapter.out.persistence")
 public class KronosApplication {
 
 	public static void main(String[] args) {
