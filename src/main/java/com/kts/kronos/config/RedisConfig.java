524087fdc2176df5af9c5c838350bad7fc0cbfa1
LsaBarbosa <lsa94.contato@gmail.com>
2025-10-08 13:32:53 -0300

    redis

--- src/main/java/com/kts/kronos/config/RedisConfig.java
+++ src/main/java/com/kts/kronos/config/RedisConfig.java
@@ -7,23 +7,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            LettuceConnectionFactory connectionFactory
+
+    ) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        GenericJackson2JsonRedisSerializer jsonRedisSerializer =
-                new GenericJackson2JsonRedisSerializer(objectMapper);
+        RedisSerializer<Object> jsonRedisSerializer = RedisSerializer.json();
 
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(jsonRedisSerializer); // 5. Atribua o novo serializador
+        template.setValueSerializer(jsonRedisSerializer);
 
+        // Opcional: Para garantir que hash keys/values também usem o serializador correto
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(jsonRedisSerializer);
+
+        template.afterPropertiesSet();
         return template;
     }
 }
