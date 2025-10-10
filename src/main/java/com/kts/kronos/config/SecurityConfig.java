1144d211a792db7b83669e31ea50955eba6286ab
LsaBarbosa <lsa94.contato@gmail.com>
2025-10-10 11:03:51 -0300

    local

--- src/main/java/com/kts/kronos/config/SecurityConfig.java
+++ src/main/java/com/kts/kronos/config/SecurityConfig.java
@@ -22,7 +22,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -65,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(recordUrl,plataformUrl));
+        configuration.setAllowedOrigins(Arrays.asList(recordUrl,plataformUrl,"http://localhost:5173"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
