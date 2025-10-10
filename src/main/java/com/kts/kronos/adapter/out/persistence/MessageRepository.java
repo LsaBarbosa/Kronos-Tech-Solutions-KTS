2fb55dbd0b38c3e8d172966e37b2bc507b59bce7
LsaBarbosa <lsa94.contato@gmail.com>
2025-10-10 10:50:22 -0300

    Message

--- src/main/java/com/kts/kronos/adapter/out/persistence/MessageRepository.java
+++ src/main/java/com/kts/kronos/adapter/out/persistence/MessageRepository.java
@@ -1,6 +1,8 @@
 package com.kts.kronos.adapter.out.persistence;
 
 import com.kts.kronos.adapter.out.persistence.entity.MessageEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -13,4 +15,15 @@ public interface MessageRepository extends JpaRepository<MessageEntity, UUID> {
     void deleteByMessageIdAndEmployeeId(UUID messageId, UUID employeeId);
     @Transactional
     void deleteByCreatedAtBefore(LocalDateTime threshold);
+
+    @Query("""
+        SELECT m FROM MessageEntity m
+        WHERE m.companyId = :companyId
+        AND (m.recipientEmployeeId IS NULL OR m.recipientEmployeeId = :employeeId)
+        ORDER BY m.createdAt DESC
+    """)
+    List<MessageEntity> findVisibleMessagesByCompanyIdAndEmployeeId(
+            @Param("companyId") UUID companyId,
+            @Param("employeeId") UUID employeeId
+    );
 }
--- src/main/java/com/kts/kronos/domain/model/Message.java
+++ src/main/java/com/kts/kronos/domain/model/Message.java
@@ -11,8 +11,22 @@ public record Message(UUID messageId,
                       String title,
                       String messageText,
                       MessagePriority priority,
-                      LocalDateTime createdAt) {
-    public Message(UUID employeeId, UUID companyId,String title, String messageText, MessagePriority priority) {
-        this(UUID.randomUUID(), employeeId, companyId,title, messageText, priority, LocalDateTime.now());
+                      LocalDateTime createdAt,
+                      UUID recipientEmployeeId
+) {
+    public Message(UUID messageId, UUID employeeId, UUID companyId, String title, String messageText, MessagePriority priority, LocalDateTime createdAt, UUID recipientEmployeeId) {
+        this.messageId = messageId;
+        this.employeeId = employeeId;
+        this.companyId = companyId;
+        this.title = title;
+        this.messageText = messageText;
+        this.priority = priority;
+        this.createdAt = createdAt;
+        this.recipientEmployeeId = recipientEmployeeId;
+    }
+
+    // Construtor para criação (usado no Service)
+    public Message(UUID employeeId, UUID companyId, String title, String messageText, MessagePriority priority, UUID recipientEmployeeId) {
+        this(UUID.randomUUID(), employeeId, companyId, title, messageText, priority, LocalDateTime.now(), recipientEmployeeId);
     }
 }
