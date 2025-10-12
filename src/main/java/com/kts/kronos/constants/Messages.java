3930401a7dff6205c552a6ab356b4c6dbc4e3ae0
LsaBarbosa <lsa94.contato@gmail.com>
2025-10-12 12:15:49 -0300

    local

--- src/main/java/com/kts/kronos/constants/Messages.java
+++ src/main/java/com/kts/kronos/constants/Messages.java
@@ -115,47 +115,5 @@ public static final String RECORD_NOT_BELONGS_EMPLOYEE = "Desculpe!, Você não 
 
     // **
     public static final String NOT_ABLE_TO_READ_FILE = "Não foi possível ler o arquivo: ";
-    public static final String EMAIL_CONTENT = "<!DOCTYPE html>" +
-            "<html lang='pt-BR'>" +
-            "<head><meta charset='UTF-8'></head>" +
-            "<body style='font-family: Arial, sans-serif; color: #333; line-height: 1.6;'>" +
 
-            // APRESENTAÇÃO
-            "<h1 style='color: #0056b3; font-size: 24px;'>Olá, %s! Sua Segurança é Nossa Prioridade.</h1>" +
-            "<hr style='border: 0; border-top: 1px solid #eee;'>" +
-
-            // CORPO
-            "<p style='font-size: 16px;'>Recebemos uma solicitação para **redefinir a senha** da sua conta.</p>" +
-            "<p style='font-size: 16px;'>Para criar uma nova senha, clique no botão abaixo. Se você não consegue clicar, copie e cole o link no seu navegador.</p>" +
-
-            // DESTAQUE E BOTÃO
-            "<p style='margin: 25px 0;'>" +
-            // Botão com fundo azul, texto branco, fonte maior e negrito
-            "<a href='%s' target='_blank' style='" +
-            "display: inline-block; " +
-            "padding: 12px 25px; " +
-            "background-color: #007bff; " +
-            "color: #ffffff; " +
-            "text-decoration: none; " +
-            "border-radius: 5px; " +
-            "font-size: 18px; " +
-            "font-weight: bold;" +
-            "'>Criar Nova Senha</a>" +
-            "</p>" +
-
-            // INFORMAÇÃO DE EXPIRAÇÃO (ALERTA EM LARANJA)
-            "<p style='font-size: 14px; color: #ff9900; margin-top: 20px;'>" +
-            "**Lembre-se:** Este link de redefinição **expira em 30 minutos** por questões de segurança." +
-            "</p>" +
-
-            // CLÁUSULA DE SEGURANÇA
-            "<p style='font-size: 14px; color: #666; margin-top: 30px;'>" +
-            "Se você **não solicitou** a redefinição de senha, por favor, **ignore** este e-mail. Nenhuma ação será tomada na sua conta." +
-            "</p>" +
-
-            // FECHAMENTO
-            "<p style='font-size: 14px; margin-top: 40px;'>Atenciosamente,<br>O Time de Suporte [Nome da Sua Empresa]</p>" +
-
-            "</body>" +
-            "</html>";
 }
