618784d2426ccbba597e4c8e64deac6e2bbb962e
LsaBarbosa <lsa94.contato@gmail.com>
2026-04-16 23:11:06 -0300

    Teste container

---
+++ src/test/java/com/kts/kronos/adapter/out/persistence/CompanyNsrRepositoryDataJpaTest.java
@@ -0,0 +1,32 @@
+package com.kts.kronos.adapter.out.persistence;
+
+import com.kts.kronos.support.jpa.AbstractPostgresContainerTest;
+import com.kts.kronos.support.jpa.PostgresDataJpaTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@PostgresDataJpaTest
+class CompanyNsrRepositoryDataJpaTest extends AbstractPostgresContainerTest {
+
+    @Autowired
+    private CompanyNsrRepository repository;
+
+    @Test
+    @DisplayName("incrementAndGetNsr: deve iniciar em 1 e incrementar atomicamente")
+    void shouldIncrementNsrUsingPostgresUpsert() {
+        UUID companyId = UUID.randomUUID();
+
+        Long first = repository.incrementAndGetNsr(companyId);
+        Long second = repository.incrementAndGetNsr(companyId);
+        Long third = repository.incrementAndGetNsr(companyId);
+
+        assertEquals(1L, first);
+        assertEquals(2L, second);
+        assertEquals(3L, third);
+    }
+}
