711a76598cd18f7a8a44860ebd593788fea8616f
LsaBarbosa <lsa94.contato@gmail.com>
2026-04-19 12:03:42 -0300

    fix test
    adição de tests no repository

--- src/test/java/com/kts/kronos/adapter/out/persistence/DocumentRepositoryTest.java
+++ src/test/java/com/kts/kronos/adapter/out/persistence/DocumentRepositoryTest.java
@@ -13,7 +13,6 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataJpaTest
 class DocumentRepositoryTest extends AbstractPostgresDataJpaTest {
 
     @Autowired
--- src/test/java/com/kts/kronos/adapter/out/persistence/EmployeeRepositoryTest.java
+++ src/test/java/com/kts/kronos/adapter/out/persistence/EmployeeRepositoryTest.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+
 class EmployeeRepositoryTest extends AbstractPostgresDataJpaTest {
 
     @Autowired
